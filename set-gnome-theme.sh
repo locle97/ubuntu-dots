8115845a0a242edffa1d3dabf72ccba780efa500
Loc Le <lpl212757@gmail.com>
2025-04-26 20:16:27 +0700

    Set gnome theme

--- set-gnome-theme.sh
+++ set-gnome-theme.sh
@@ -1,2 +1,8 @@
-source ~/.local/share/omakub/themes/tokyo-night/gnome.sh
-source ~/.local/share/omakub/themes/tokyo-night/tophat.sh
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface gtk-theme "Yaru-magenta-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru-magenta"
+gsettings set org.gnome.desktop.interface accent-color "magenta" 2>/dev/null || true
+
+# Tophat
+gsettings set org.gnome.shell.extensions.tophat meter-fg-color "#e920a3"
