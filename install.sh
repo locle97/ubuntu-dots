01c330ab7c4eb9c6e833153be6a3f127f33af79b
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2025-06-13 23:16:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl git unzip
 
+# Clone Omakub first
+git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+
 # Install required softwares
 for installer in ./required/*.sh; do source $installer; done
 
