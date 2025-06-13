8ab786ab3312cde230a04ed5baa9f205b2b044e9
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2025-06-13 23:18:17 +0700

    Update set-gnome-settings.sh

--- apps/set-gnome-settings.sh
+++ apps/set-gnome-settings.sh
@@ -6,3 +6,6 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Ner
 
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# Disable eoverlay-key
+gsettings set org.gnome.mutter overlay-key ''
